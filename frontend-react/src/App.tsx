6d825db01faeb50cbeb8b8232d17c10fcf7d6b7a
Siyabulela <S.Mgijima@plum.systems>
2025-09-05 05:10:53 +0200

    PlacementData, Position, Scale, Rotation frontend

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -3,7 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TestApi from "./views/TestApi";
 import CustomerTestApi from "./views/CustomerTestApi"; // <-- import your new file
 import CustomerDemo from "./components/CustomerDemo";
-import TShirtDesigner from "./components/TShirtDesigner";
+// <<<<<<< HEAD
+// import TShirtDesigner from "./components/TShirtDesigner";
+=======
+import ScaleTestApi from "./views/ScaleTestApi";
+import PositionTestApi from "./views/PositionTestApi";
+import RotationTestApi from "./views/RotationTestApi";
+import PlacementDataTestApi from "./views/PlacementDataTestApi";
+// >>>>>>> c746059 (PlacementData, Position, Scale, Rotation frontend)
 
 function App() {
     return (
@@ -12,7 +19,14 @@ function App() {
                 <Route path="/test-api" element={<TestApi />} />
                 <Route path="/customer-test-api" element={<CustomerTestApi />} />
                 <Route path="/customer-demo" element={<CustomerDemo />} />
+<<<<<<< HEAD
                 <Route path="/tshirt-designer" element={<TShirtDesigner />} /> {/* 👈 new route */}
+=======
+                <Route path="/placement-test-api" element={<PlacementDataTestApi />} />
+                <Route path="/scale-test-api" element={<ScaleTestApi />} />
+                <Route path="/position-test-api" element={<PositionTestApi />} />
+                <Route path="/rotation-test-api" element={<RotationTestApi />} />
+>>>>>>> c746059 (PlacementData, Position, Scale, Rotation frontend)
                 {/* <-- new route */}
             </Routes>
         </Router>
---
+++ frontend-react/src/domain/Scale.ts
@@ -0,0 +1,6 @@
+export interface Scale {
+    scaleId?: number;
+    x: number;
+    y: number;
+    z: number;
+}
